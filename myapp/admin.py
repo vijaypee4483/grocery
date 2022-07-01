80aa9b76608061bf6f85cad0ef3bf751347434b0
Vijay <vp.gd.msw@gmail.com>
2022-07-01 17:57:55 +0530

    New model is added in app

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-from .models import Category, Customer
+from .models import Category, Customer, Product, Order
 
 # Register your models here.
 @admin.register(Category)
 class Request_category(admin.ModelAdmin):
-    list_display = [field.name for field in Category._meta.get_fields()]
+    list_display = ['name']
 
 @admin.register(Customer)
 class Request_customer(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'phone', 'email', 'password']
+
+@admin.register(Product)
+class Request_products(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category', 'description', 'image']
+
+@admin.register(Order)
+class Request_orders(admin.ModelAdmin):
+    list_display = ['product', 'customer', 'quantity', 'price', 'address', 'phone', 'date', 'status']
--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,6 @@
-import email
-from re import M
+from datetime import datetime
 from django.db import models
-from more_itertools import first, last
+
 
 # Create your models here.
 class Category(models.Model):
@@ -36,3 +35,48 @@ class Customer(models.Model):
         if Customer.objects.filter(email=self.email):
             return True
         return False
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description =models.CharField(max_length=200, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/productbs')
+
+    @staticmethod
+    def get_product_by_id(ids):
+        return Product.objects.filter(id__in=ids)
+
+    @staticmethod
+    def get_product_by_name(names):
+        return Product.objects.filter(name__in=names)
+
+    @staticmethod
+    def get_all_products():
+        return Product.objects.all()
+
+    @staticmethod
+    def get_all_product_by_category_id(category_id):
+        if category_id:
+            return Product.objects.filter(category=category_id)
+        return Product.get_all_products()
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price = models.IntegerField()
+    address = models.CharField(max_length=60, default='', null=True)
+    phone = models.CharField(max_length=10, default='', null=True)
+    date = models.DateField(default= datetime.today())
+    time = models.TimeField(default=datetime.now())
+    status = models.BooleanField(default=False)
+
+    def place_order(self):
+        self.save()
+
+    @staticmethod
+    def get_order_by_customer(customer_id):
+        return Order.objects.filter(customer=customer_id).order_by('-date')
